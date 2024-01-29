5a6a667d8304a54ee80b0444b9bbe6e068b62a64
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2024-01-29 16:23:06 -0500

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Assignment3_bigger_calculator"
+include(":app")
+ 
